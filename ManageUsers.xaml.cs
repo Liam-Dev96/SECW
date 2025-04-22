a6c0fa9d580fd14f99946bffa951a32327553f10
Ahmed Mursal <amursal908@gmail.com>
2025-04-22 15:00:35 +0100

    Add ManageUsers page with user input fields and role selection

--- ManageUsers.xaml.cs
+++ ManageUsers.xaml.cs
@@ -1,20 +1,12 @@
-using System;
 using Microsoft.Maui.Controls;
-using System.Data.SQLite;
-using BCrypt.Net;
-using SECW.Helpers;
 
 namespace SECW
 {
     public partial class ManageUsers : ContentPage
     {
-        
+        public ManageUsers()
+        {
+            InitializeComponent();
+        }
     }
-    
-    
-    
-
-
-
-
 }
